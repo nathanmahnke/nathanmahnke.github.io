b37528594bd9fa5ee39832deb0a879b67493494d
nathanmahnke <62089310+nathanmahnke@users.noreply.github.com>
2025-05-14 00:17:53 +0000

    Deploying to gh-pages from @ nathanmahnke/nathanmahnke.github.io@e9a5fb1ad71ae53ca4e73762d7efa496813c6dd9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,18 +17,18 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
-          title: "repositories",
+          title: "Repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-resume",
+          title: "Resume",
+          description: "Below, you will find an easily navigable version of my resume. If you find yourself in need of a pdf copy, click the pdf icon to the top right.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/resume/";
           },
         },{id: "post-a-post-with-plotly-js",
         
