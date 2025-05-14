2d5d96e5e229f2aa38d63189795aa8b144c52346
nathanmahnke <62089310+nathanmahnke@users.noreply.github.com>
2025-05-14 00:20:30 +0000

    Deploying to gh-pages from @ nathanmahnke/nathanmahnke.github.io@74ee858cab091cce7e0517022f4c85b03b2fb772 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-demos",
+          title: "Demos",
+          description: "A handy place to try out my demos",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/demos/";
+          },
+        },{id: "nav-projects",
           title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
